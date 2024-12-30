399d2dc71835b087f83de907ede4d0a7e2f09537
Calvin Ludwig <git@calvinludwig.xyz>
2024-12-30 17:56:52 -0300

    favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 const config: Config = {
   title: "Calvin Ludwig",
   tagline: "Software Engineer",
-  favicon: "img/favicon.ico",
+  favicon: "https://github.com/calvinludwig.png",
 
   // Set the production url of your site here
   url: "https://calvinludwig.netlify.app",
